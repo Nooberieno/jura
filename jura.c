8d78ba2e7a1b56aa828fafb3d0314bb2d4d21f84
Nooberieno <124163902+Nooberieno@users.noreply.github.com>
2024-03-01 12:18:43 +0100

    Added support for a bunch of programming languages and programming language related formats like HTML CSS and JSON

--- jura.c
+++ jura.c
@@ -44,8 +44,8 @@ enum Highlight{
 	HL_MATCH
 };
 
-#define HL_HIGHLIGHT_NUMBERS (1<<0)
-#define HL_HIGHLIGHT_STRINGS (1<<1)
+#define HighlightDigits (1<<0)
+#define HighlightStrings (1<<1)
 
 /* data */
 
@@ -96,17 +96,208 @@ char *CKeywords[] = {
 	"int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|", "void|", NULL
 };
 
-struct Syntax HLDB[] = {
+char *CppExtensions[] = {".cpp", ".CPP", ".cc", ".cp", ".cxx", ".C", ".hh", ".hpp", NULL};
+char *CppKeywords[] = {
+	"switch", "if", "while", "for", "break", "continue", "return", "else", "struct", "union", "typedef", "static", "enum", "class", "case", "alignas", "alignof",
+	"and", "and_eq", "asm", "bitand", "bitor", "class", "compl", "constexpr", "const_cast", "deltype", "delete", "dynamic_cast", "explicit", "export", "false",
+	"friend", "inline", "mutable", "namspace", "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected", "public",
+	"reinterpret_cast", "static_assert", "static_cast", "template", "this", "thread_local", "throw", "true", "try", "typeid", "typename", "virtual", "xor", "xor_eq",
+	"int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|", "void|", NULL
+};
+char *PythonExtensions[] = {".py", NULL};
+char *PythonKeywords[] = {
+	"False", "None", "True", "and", "as", "assert", "async", "await", "await", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for",
+	"for", "from", "global", "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try", "while", "with", "yield",
+	"int|", "float|", "complex|", "bool|", "str|", "list|", "tuple|", "set|", "dict|", "NoneType|", NULL
+};
+char *LuaExtensions[] = {".lua", NULL};
+char *LuaKeywords[] = {
+	"and", "break", "do", "else", "elseif", "end", "false", "for", "function", "if", "in", "local", "nil", "not", "or", "repeat", "return", "then", "true", "until", "while",
+	"nil|", NULL
+};
+char *RustExtensions[] = {".rs", NULL};
+char *RustKeywords[] = {
+	"as", "async", "await", "break", "const", "continue", "crate", "dyn", "else", "enum", "extern", "false", "fn", "for", "if", "impl", "in", "let", "loop", "match",
+	"mod", "move", "mut", "pub", "ref", "return", "self", "static", "struct", "super", "trait", "true", "type", "unsafe", "use", "where", "while",
+	"bool|", "char|", "i8|", "i16|", "i32|", "i64|", "i128|", "u8|", "u16|", "u32|", "u64|", "u128|", "f32|", "f64|", "str|", "tuple|", "array|", "slice|", "pointer|",
+	"reference|", "Option|", "Result|", "struct|", "enum|", "trait|", "impl|", NULL 
+};
+char *JSExtensions[] = {".js", NULL};
+char *JSKeywords[] = {
+	"break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "export", "extends", "finally", "for", "function", "if",
+	"import", "in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield", NULL
+};
+char *TSExtensions[] = {"ts", ".d.ts", NULL};
+char *TSKeywords[] = {
+	"break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "export", "extends", "finally", "for", "function", "if",
+	"import", "in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield", "type", "interface",
+	"implements", "enum", "readonly",
+	"undefined|", "null|", "boolean|", "number|", "string|", "object|", "symbol|", "BigInt|", "any|", NULL
+};
+char *CSExtensions[] = {".cs", NULL};
+char *CSKeywords[] = {
+	"abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "default", "delegate", "do", "else", "enum",
+	"event", "explicit", "extern", "false", "finally", "fixed", "for", "foreach", "goto", "if", "implicit", "in", "interface", "internal", "is", "lock", "namespace",
+	"new", "object", "operator", "out", "override", "params", "privte", "protected", "public", "readonly", "sbyte", "sealed", "short", "sizeof", "stackalloc", "statoc",
+	"struct", "switch", "this", "throw", "true", "typeof", "unchecked", "unsafe", "using", "var", "virtual", "void", "volatile", "while", "yield",
+	"bool|", "byte|", "char|", "decimal|", "double|", "float|", "int|", "long|", "object|", "sbyte|", "short|", "short|", "string|", "uint|", "ulong|", "ushort|", NULL
+};
+char *JavaExtensions[] = {".java", NULL};
+char *JavaKeywords[] = {
+	"abstract", "assert", "break", "case", "catch", "class", "continue", "default", "do", "else", "enum", "extends", "final", "finally", "for", "if", "implements",
+	"import", "instanceof", "interface", "native", "now", "package", "private", "protected", "public", "return", "static", "strictfp", "super", "switch", "syncronized",
+	"this", "throw", "throws", "transient", "try", "void", "volatile", "while",
+	"boolean|", "byte|", "char|", "double|", "float|", "int|", "long|", "short|", NULL
+};
+char *GolangExtensions[] = {".go", ".mod", ".sum", NULL};
+char *GoKeywords[] = {
+	"break", "case", "chan", "const", "continue", "default", "defer", "else", "fallthrough", "for", "func", "go", "if", "import", "interface", "map", "package", "range", "return", "select", "struct", "switch", "type", "var",
+	"bool|", "byte|", "complex64|", "complex128|", "error|", "float32|", "float64|", "int|", "int8|", "int16|", "int32|", "int64|", "rune|", "string|", "uint|", "uint8|", "uint16|", "uint32|", "uint64|", "uintptr|", NULL
+
+};
+char *SwiftExtensions[] = {".swift", ".xcodeproj", ".storyboard", ".xib", ".xcdatamodeld", NULL};
+char *SwiftKeywords[] = {
+	"associatedtype", "break", "case", "catch", "class", "continue", "convenience", "deinit", "didSet", "do", "else", "enum", "extension", "fallthrough", "final", "for", "func", "guard", "if", "import", "in", "indirect", "init", "inout", "internal", "is", "as", "lazy", "let", "localized", "mutating", "nil", "nonce", "nonisolated", "objc", "open", "optional", "override", "postfix", "precedencegroup", "private", "protocol", "public", "required", "rethrows", "return", "self", "set", "some", "static", "struct", "subscript", "super", "switch", "synchronized", "throw", "throws", "try", "typealias", "var", "weak", "where", "while", "willSet",
+	"Bool|", "Double|", "Float|", "Int|", "Int8|", "Int16|", "Int32|", "Int64|", "String|", "UInt|", "UInt8|", "UInt16|", "UInt32|", "UInt64|", "Character|", "Any|", "AnyObject|", "Array|", "Dictionary|", "FloatingPointNumber|", "Hashable|", "IntegerLiteralConvertible|", "Numeric|", "Optional|", "Range|", "RawRepresentable|", "Sequence|", "SignedNumeric|", "Strideable|", "Subscript|", "Comparable|", "Codable|", "Decodable|", "Encodable|", "Error|", "UnsafeMutableRawPointer|", "UnsafeMutableRawBufferPointer|", "UnsafePointer|", "UnsafeRawBufferPointer|", NULL
+};
+char *HTMLExtensions[] = {".html", NULL};
+char *HTMLKeywords[] = {
+	"<!DOCTYPE html>",
+	"<html>", "<head>", "<title>", "</title>", "</head>", "<body>", "</body>", "</html>",
+	"<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>", "</h1>", "</h2>", "</h3>", "</h4>", "</h5>", "<h6>",
+	"<p>", "</p>",
+	"<ul>", "<ol>", "<li>", "</li>", "</ul>", "</ol>",
+	"<img src=\"...\"/>", "<a href=\"...\"></a>",
+	"<b>", "<i>", "<u>", "<sup>", "<sub>", "</b>", "</i>", "</u>", "</sup>", "<sub>",
+	"<table>", "<tr>", "<th>", "<td>", "</table>", "<tr>", "<th>", "<td>",
+	"<form>", "<input type=\"...\"/>", "<button>", "</form>", "<script>", "</script>"
+	"<br/>", "<hr/>", NULL
+};
+char *JSONExtensions[] = {".json", NULL};
+char *JSONSyntax[] = {
+	"{", "}", "[", "]", ",", ":", NULL
+};
+char *CssExtensions[] = {".css", NULL};
+char *CssSyntax[] = {
+	/* Selectors */
+"body", "*", "h1", "h2", "h3", "h4", "h5", "h6", "p", "a", "ul", "ol", "li", "img", "table", "tr", "th", "td", "form", "input", "button", ".", "#...",
+
+/* Properties */
+"background-color", "color", "font-family", "font-size", "font-weight", "text-align", "margin", "padding", "border", "width", "height", "display", "float", "flex", "grid", "position", "z-index",
+
+/* Values */
+"inherit", "initial", "auto", "none",
+"transparent", "black", "white", "red", "green", "blue", "yellow", "purple", "orange", "teal", "pink", "gray",
+"Arial", "Verdana", "Helvetica", "Times New Roman", "Courier New",
+"12px", "16px", "20px", "24px",
+"bold", "italic", "normal",
+"left", "center", "right", "justify",
+"10px", "20px", "auto",
+"display: block", "display: inline", "display: flex", NULL
+};
+
+struct Syntax SyntaxDatabase[] = {
 	{
 		"c",
 		CExtensions,
 		CKeywords,
 		"//", "/*", "*/",
-		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
+		HighlightDigits | HighlightStrings
 	},
+	{
+		"c++",
+		CppExtensions,
+		CppKeywords,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"Python",
+		PythonExtensions,
+		PythonKeywords,
+		"#", "...", "...",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"Lua",
+		LuaExtensions,
+		LuaKeywords,
+		"--", "--[[", "]]--",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"Rust",
+		RustExtensions,
+		RustKeywords,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"JavaScript",
+		JSExtensions,
+		JSKeywords,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"TypeScript",
+		TSExtensions,
+		TSKeywords,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"C#",
+		CSExtensions,
+		CSKeywords,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"Java",
+		JavaExtensions,
+		JavaKeywords,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"GO",
+		GolangExtensions,
+		GoKeywords,
+		"//", "/*", "*/",
+		HighlightDigits| HighlightStrings
+	},
+	{
+		"Swift",
+		SwiftExtensions,
+		SwiftKeywords,
+		"//", "/*", "*/",
+		HighlightDigits| HighlightStrings
+	},
+	{
+		"HTML",
+		HTMLExtensions,
+		HTMLKeywords,
+		NULL, NULL, NULL,
+		HighlightDigits | HighlightStrings
+	},
+	{
+		"JSON",
+		JSONExtensions,
+		JSONSyntax,
+		NULL, NULL, NULL,
+		HighlightStrings
+	},
+	{
+		"css",
+		CssExtensions,
+		CssSyntax,
+		"//", "/*", "*/",
+		HighlightDigits | HighlightStrings
+	}
 };
 
-#define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
+#define SyntaxDatabaseEntries (sizeof(SyntaxDatabase) / sizeof(SyntaxDatabase[0]))
 
 /* prototypes */
 
@@ -253,7 +444,7 @@ void UpdateSyntax(eline *line){
 				continue;
 			}
 		}
-		if(config.syntax->flags & HL_HIGHLIGHT_STRINGS){
+		if(config.syntax->flags & HighlightStrings){
 			if(in_string){
 				line->hl[i] = HL_STRING;
 				if(c == '\\' && i + 1 < line->rendersize){
@@ -274,7 +465,7 @@ void UpdateSyntax(eline *line){
 				}
 			}
 		}
-		if(config.syntax->flags & HL_HIGHLIGHT_NUMBERS){
+		if(config.syntax->flags & HighlightDigits){
 			if((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
 				line->hl[i] = HL_NUMBER;
 				i++;
@@ -325,8 +516,8 @@ void SelectSyntaxHighlight(){
 	config.syntax = NULL;
 	if(config.filename == NULL) return;
 	char *ext = strrchr(config.filename, '.');
-	for(unsigned int j = 0; j < HLDB_ENTRIES; j++){
-		struct Syntax *s = &HLDB[j];
+	for(unsigned int j = 0; j < SyntaxDatabaseEntries; j++){
+		struct Syntax *s = &SyntaxDatabase[j];
 		unsigned int i = 0;
 		while(s->filematch[i]){
 			int is_ext = (s->filematch[i][0] == '.');
